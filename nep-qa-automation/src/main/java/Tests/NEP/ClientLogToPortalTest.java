b687f5418abcd22df7a1243b528102951354cc6e
Uri Hacohen <uri.hacohen@trustwave.com>
2020-06-21 11:02:42 +0300

    Updated log debug messages

--- nep-qa-automation/src/main/java/Tests/NEP/ClientLogToPortalTest.java
+++ nep-qa-automation/src/main/java/Tests/NEP/ClientLogToPortalTest.java
@@ -21,30 +21,21 @@ public class ClientLogToPortalTest extends GenericTest {
 
     @Test( groups = { "logs" } )
     public void SendLogsAndVerify () {
-
-        JLog.logger.debug("Before Report Interval");
-
+        JLog.logger.debug("Test Started. log entry to appear at portal timeout: " + data.get("Log To Appear Timeout"));
         action.ChangeReportInterval(data.get("Report Interval"));
-        JLog.logger.debug("After Report Interval");
 
         LogEntry entry = new LogEntry(data.get("Event Type"),data.get("Event ID"),data.get("Event Log"),data.get("Event Source"),data.get("Event Description"),data.get("Add time stamp to description"));
-        JLog.logger.debug("After new log entry");
-
         action.WriteEvent(entry);
-        JLog.logger.debug("After write event");
-
 
         action.LaunchApplication(general.get("Browser"));
-        JLog.logger.debug("After launch  application: " + general.get("Browser"));
-
         action.SetApplicationUrl(data.get("Fusion Link"));
-        JLog.logger.debug("After set application url: " + data.get("Fusion Link"));
 
         action.Login(data.get("Fusion User Name"), data.get("Fusion Password"));
         action.GotoEventExplorer(data.get("Fusion Link"));
 
         action.SelectCustomer(data.get("Customer"));
         action.VerifyMessageExistsInPortal(entry,Integer.parseInt(data.get("Log To Appear Timeout")));
+        JLog.logger.debug("Test ended");
 
     }
 
