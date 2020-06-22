93c91e54beff3d0d1f6d8fce118f727377e9fcff
Uri Hacohen <uri.hacohen@trustwave.com>
2020-06-22 11:24:21 +0300

    Better test messages in case of success / failure

--- nep-qa-automation/src/main/java/Tests/NEP/Lenny/SetConfigurationTest.java
+++ nep-qa-automation/src/main/java/Tests/NEP/Lenny/SetConfigurationTest.java
@@ -26,8 +26,6 @@ public class SetConfigurationTest extends GenericTest {
     public void SetConfiguration () {
 
         //Short POC test
-
-        //String myJson = "{ \"customerId\": 1007, \"configuration\": { \"global_conf\": { \"log_level\": \"info\", \"conf_version\": 3 }, \"agent\": { \"ds_host\": \"endpoint-protection-services.local.tw-test.net\", \"ds_port\": 443, \"ds_protocol\": \"https\", \"check_update_period\": 61, \"report_period\": 60, \"ds_max_off_period\": 48, \"modules\": [{ \"name\": \"Windows Log Monitor\", \"binary_name\": \"WLM.dll\", \"enabled\": true }, { \"name\": \"Log File Monitor\", \"binary_name\": \"LFM.dll\", \"enabled\": true } ], \"transport\": { \"transport_type\": 2, \"syslog\": { \"port\": 0 }, \"scp\": { \"host\": \"siem-ingress.trustwave.com\", \"dest_folder\": \"/var/siem/data/nep\", \"port\": 9022, \"user\": \"twsiem\", \"ack\": false, \"max_send_folder_size\": 100 } } }, \"wlm\": { \"max_monitor_queue_size\": 10000, \"queues_collector_idle_time\": 5, \"monitor_items\": [{ \"log_name\": \"Security\", \"enabled\": true, \"advanced_filter\": false, \"filters\": [] }, { \"log_name\": \"System\", \"enabled\": true, \"advanced_filter\": false, \"filters\": [] } ] }, \"lfm\": { \"max_monitor_queue_size\": 10000, \"queues_collector_idle_time\": 5, \"monitor_items\": [] } } }";
         String myJson = data.get("Settings Json");
         String lennyIP = PropertiesFile.readProperty("ClusterToTest");
 
@@ -40,7 +38,11 @@ public class SetConfigurationTest extends GenericTest {
                         post("initCustomerSettings");
 
         int response = r.getStatusCode();
-        JLog.logger.info("Set configuration response: " + response);
+
+        if(response==200)
+            JLog.logger.info("Success. Set configuration response: " + response);
+        else
+            org.testng.Assert.fail("Error. Response status code received is: " + response);
 
         //Second method of posting and verifying response
         given()
@@ -49,17 +51,6 @@ public class SetConfigurationTest extends GenericTest {
                 when().
                 post("initCustomerSettings").
                 then().statusCode(200);
-
-
     }
-
-    @AfterMethod
-    public void Close() throws Exception {
-        afterMethod();
-        action.CloseApplication();
-
-    }
-
-
 
 }
