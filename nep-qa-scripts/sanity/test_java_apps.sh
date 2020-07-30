c720593427a65272b16bf353395dd7d23aec3b20
Ilya Valevich <ivalevich@trustwave.com>
2020-07-30 12:02:46 +0300

    fix script to test java apps readiness after LNE install

--- nep-qa-scripts/sanity/test_java_apps.sh
+++ nep-qa-scripts/sanity/test_java_apps.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-echo "Tesing Springboot apps =========="
-apps_ports=$(find /work/services/nepa-services /work/services/stub-srv/etc/dummy-portal-services -name application.properties -exec grep server.port {} \; | cut -d= -f2)
+echo "Testing Springboot apps =========="
+apps_ports=$(find /work/services/nepa-services\
+                  /work/services/stub-srv/etc/dummy-portal-services \
+                  -name application.properties \
+                  -exec grep server.port {} \; | cut -d= -f2)
+
 err=1
 retries=1
 max_retries=10
+sleep_time=30
+temp_file=/tmp/port_status
 while [ $err -ne 0 ] && [ ! $retries -gt $max_retries ]
 do
     echo "Trying ($retries/$max_retries)"
-    echo "Sleeping 30 secs ..." && sleep 30
+    echo "Sleeping ${sleep_time} secs ..." && sleep $sleep_time
     err=0
-    for i in $apps_ports 123
+    rm -f $temp_file
+    echo -n "Checking port $i ... "
+    for i in $apps_ports
     do
-        echo -n "Checking port $i ... "
-        curl -s http://localhost:$i/healthcheck/quick | grep -sq '"appStatus":"OK"' && echo PASS && continue
+        curl -s http://localhost:$i/healthcheck/quick | grep -sq '"appStatus":"OK"' \
+            && echo PASS \
+            && echo "port $i PASSED" >> $temp_file \
+            && continue
         echo FAILED
-        err=1
+        echo "port $i FAILED" >> $temp_file
     done
     ((++retries))
 done
+
+if grep -sq 'FAILED' $temp_file; then
+    echo "test java apps port availability NOTOK"
+    cat $temp_file
+else
+    echo "test java apps port availability OK"
+fi
