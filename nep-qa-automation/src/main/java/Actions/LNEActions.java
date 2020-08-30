117e5b2e02cc694eb444b2020d1c22fe5f3128db
Roman Bar <rbar@trustwave.com>
2020-08-30 23:53:51 +0300

    Code refactoring
    NEP-1255 - Rollback in case binary update failed - phase 1

--- nep-qa-automation/src/main/java/Actions/LNEActions.java
+++ nep-qa-automation/src/main/java/Actions/LNEActions.java
@@ -22,6 +22,7 @@ public class LNEActions extends ManagerActions  {
     public static final String basePrefix = "http://";
     public static final String baseSuffix = ":9091/nep-centcom-client/";
     public static final String backupIdentifier = "backup";
+    public static final String hashIdentifier = "sha256";
     public static final String nepa_caDotPemPath = "C:/Program Files/Trustwave/NEPAgent/certs/nepa_ca.pem";
     public static final String lneFileCabinetPath = "/work/services/stub-srv/var/file_cabinet/";
     public static final String caCertificateFileName = "cacertificate.txt";
@@ -229,7 +230,7 @@ public class LNEActions extends ManagerActions  {
             found = false;
             while (durationTimeout.compareTo(Duration.between(start, current)) > 0) {
                 for (int i = 0; i < list.size(); i++) {
-                    if (list.get(i).contains(linuxInstallationFile) && !list.get(i).contains(backupIdentifier)) {
+                    if (list.get(i).contains(linuxInstallationFile) && !list.get(i).contains(backupIdentifier) && !list.get(i).contains(hashIdentifier)) {
                         String source = clientFolder + "/" + list.get(i);
                         String destination = copyLinuxInstallerLocation;
                         connection.CopyToLocal(source, destination);
--- nep-qa-automation/src/main/java/Tests/LNE/BinaryUpdate.java
+++ nep-qa-automation/src/main/java/Tests/LNE/BinaryUpdate.java
@@ -45,7 +45,31 @@ public class BinaryUpdate extends GenericTest {
     }
 
     @Test()
-    public void CheckBinaryUpdate() throws IOException, InterruptedException {
+    public void CheckSuccessfulBinaryUpdate() throws IOException, InterruptedException {
+
+        String windowsNewVer = data.get("update_win_ver");
+        String linuxNewVer = data.get("update_linux_ver");
+        try {
+            BasicBinaryUpdateFlow(windowsNewVer, linuxNewVer);
+
+            VerifySuccessfulUpdate(windowsNewVer, linuxNewVer);
+
+            JLog.logger.info("Done...");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            org.testng.Assert.fail("Failure in CheckSuccessfulBinaryUpdate" + "\n" + ex.toString());
+        }finally {
+            // Clean the bucket
+            CleanBucket(windowsNewVer, linuxNewVer);
+            JLog.logger.info("s3bucket cleaned...");
+        }
+    }
+
+    //@Test()
+    public void CheckBinaryUpdateFailureCorruptedExecutables() throws IOException, InterruptedException {
+/*
+        String windowsNewVer = data.get("update_bad_win_ver");
+        String linuxNewVer = data.get("update_bad_linux_ver");
 
         // restart all services
         connection = new SSHManager(general.get("LNE User Name"), general.get("LNE Password"),
@@ -69,25 +93,31 @@ public class BinaryUpdate extends GenericTest {
         endpointLinux.UninstallEndpoint(AgentActions.EP_OS.LINUX, Integer.parseInt(general.get("EP Installation timeout")));
 
         // Get newer binaries
-        String windowsNewVer = data.get("update_win_ver");
-        String linuxNewVer = data.get("update_linux_ver");
+        // FetchFilesFromNexusToLocal(windowsNewVer, linuxNewVer);
+
+        // Upload all files to s3 bucket
+        // UploadFilesToBucket(windowsNewVer, linuxNewVer);
+
+        // -----------------------------------------------------------------
+        // TEMPORARY SECTION
+
+        String s3Bucket = data.get("s3Bucket");
+        //String windowsNewVer = data.get("update_win_ver");
+        //String linuxNewVer = data.get("update_linux_ver");
 
         List<String> windowsFiles = WindowsFilesList(windowsNewVer);
         List<String> linuxFiles = LinuxFilesList(linuxNewVer);
 
-        String masterDownloadDirectory = PropertiesFile.getManagerDownloadFolder();
-        String localFolderForWinFiles = masterDownloadDirectory + "/windows/";
-        String localFolderForLinuxFiles = masterDownloadDirectory + "/linux/";
-
-        String fullPathToNexusWinVer = "https://nexus01.trustwave.com/content/repositories/releases/com/trustwave/nep/tmp/" + windowsNewVer + "/windows/";
-        String fullPathToNexusLinuxVer = "https://nexus01.trustwave.com/content/repositories/releases/com/trustwave/nep/tmp/" + linuxNewVer + "/linux/";
-
-        FetchFiles(fullPathToNexusWinVer, windowsFiles, localFolderForWinFiles);
-        FetchFiles(fullPathToNexusLinuxVer, linuxFiles, localFolderForLinuxFiles);
-
         // Create folders on LNE machine
         String winUpdFolderLNE = "/home/binaries/windows/";
         String linuxUpdFolderLNE = "/home/binaries/linux/";
+
+        //String masterDownloadDirectory = PropertiesFile.getManagerDownloadFolder();
+        //String localFolderForWinFiles = masterDownloadDirectory + "/windows/";
+        //String localFolderForLinuxFiles = masterDownloadDirectory + "/linux/";
+
+        String localFolderForWinFiles = "C:\\home\\Packages\\Win0Bytes\\";
+        String localFolderForLinuxFiles = "C:\\home\\Packages\\Linux0Bytes\\";
 
         if (!connection.IsFileExists("/home/binaries/")) {
             connection.CreateDirectory("/home/binaries/");
@@ -107,9 +137,6 @@ public class BinaryUpdate extends GenericTest {
             connection.CopyToRemote(localFolderForLinuxFiles + file, linuxUpdFolderLNE);
         }
 
-        String s3Bucket = data.get("s3Bucket");
-
-        // Upload all files to s3 bucket
         for (String file : windowsFiles) {
             response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
                     "/services/stub-srv/etc/nep-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " +
@@ -131,8 +158,12 @@ public class BinaryUpdate extends GenericTest {
             JLog.logger.info("Response: " + response);
         }
 
+        // -----------------------------------------------------------------
+        // -----------------------------------------------------------------
+
         // Add flag for s3 bucket and restart all services
         AddS3FlagAndRestartLennyServices();
+
         // Sleep for 1 minute - so ds and ds-mgmt will start
         JLog.logger.info("Waiting 1 minute for ds and ds-mgmt to start...");
         Thread.sleep(60000);
@@ -141,13 +172,13 @@ public class BinaryUpdate extends GenericTest {
         // TODO RBRB make order of EPs irrelevant
 
         endpointWin.InstallEPIncludingRequisites(AgentActions.EP_OS.WINDOWS, Integer.parseInt(general.get("EP " +
-                "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
+                        "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
                 Integer.parseInt(general.get("From EP service start until logs show EP active timeout")));
 
         JLog.logger.info("Installing linux EP...");
 
         endpointLinux.InstallEPIncludingRequisites(AgentActions.EP_OS.LINUX, Integer.parseInt(general.get("EP " +
-                "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
+                        "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
                 Integer.parseInt(general.get("From EP service start until logs show EP active timeout")));
 
         // Sleep for 2 minutes - so the EPs will check updates and be updated
@@ -165,20 +196,179 @@ public class BinaryUpdate extends GenericTest {
         else
             JLog.logger.info("Trustwave Endpoint Agent Service is running on the linux machine");
 
-        // Verify that the EPs have the correct new versions
-        String linuxBinVer = endpointLinux.getEPBinaryVersion(AgentActions.EP_OS.LINUX);
-        String winBinVer = endpointWin.getEPBinaryVersion(AgentActions.EP_OS.WINDOWS);
+        // -----------------------------------------------------------------------------------
+        //BasicBinaryUpdateFlow();
+*/
+        String windowsNewVer = data.get("update_bad_win_ver");
+        String linuxNewVer = data.get("update_bad_linux_ver");
+        try {
+            BasicBinaryUpdateFlow(windowsNewVer, linuxNewVer);
 
-        JLog.logger.info("Linux version is: " + linuxBinVer);
-        JLog.logger.info("Windows version is: " + winBinVer);
+            VerifySuccessfulRollback();
 
-        org.testng.Assert.assertEquals(linuxBinVer, linuxNewVer, "Versions don't match");
-        org.testng.Assert.assertEquals(winBinVer, windowsNewVer, "Versions don't match");
+            JLog.logger.info("Done...");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            org.testng.Assert.fail("Failure in CheckBinaryUpdateFailureCorruptedExecutables" + "\n" + ex.toString());
+        }finally {
+            // Clean the bucket
+            // CleanBucket(windowsNewVer, linuxNewVer);
+            //JLog.logger.info("s3bucket cleaned...");
+        }
+    }
 
-        // Clean the bucket
+    private void BasicBinaryUpdateFlow(String windowsNewVer, String linuxNewVer) throws InterruptedException, IOException {
+
+        // restart all services
+        connection = new SSHManager(general.get("LNE User Name"), general.get("LNE Password"),
+                PropertiesFile.readProperty("ClusterToTest"), Integer.parseInt(general.get("LNE SSH port")));
+
+//        JLog.logger.info("Restarting all services on LNE machine");
+//        String response = connection.Execute("nep_service all restart");
+//        JLog.logger.info("Response: " + response);
+//
+//        // Sleep for 1 minute - so ds and ds-mgmt will start
+//        JLog.logger.info("Waiting 1 minute for ds and ds-mgmt to start...");
+//        Thread.sleep(60000);
+
+        JLog.logger.info("Uninstalling EPs...");
+
+        endpointWin = new AgentActions(data.get("EP_HostName_1"), data.get("EP_UserName_1"), data.get("EP_Password_1"));
+        endpointLinux = new AgentActions(data.get("EP_HostName_2"), data.get("EP_UserName_2"), data.get(
+                "EP_Password_2"));
+
+        endpointWin.UninstallEndpoint(AgentActions.EP_OS.WINDOWS,Integer.parseInt(general.get("EP Installation timeout")));
+        endpointLinux.UninstallEndpoint(AgentActions.EP_OS.LINUX, Integer.parseInt(general.get("EP Installation timeout")));
+
+        // Get newer binaries
+        FetchFilesFromNexusToLocal(windowsNewVer, linuxNewVer);
+
+        // Upload all files to s3 bucket
+        UploadFilesToBucket(windowsNewVer, linuxNewVer);
+
+        // Add flag for s3 bucket and restart all services
+        AddS3FlagAndRestartLennyServices();
+
+        // Sleep for 1 minute - so ds and ds-mgmt will start
+        JLog.logger.info("Waiting 1 minute for ds and ds-mgmt to start...");
+        Thread.sleep(60000);
+
+        JLog.logger.info("Installing windows EP...");
+        // TODO RBRB make order of EPs irrelevant
+
+        endpointWin.InstallEPIncludingRequisites(AgentActions.EP_OS.WINDOWS, Integer.parseInt(general.get("EP " +
+                        "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
+                Integer.parseInt(general.get("From EP service start until logs show EP active timeout")));
+
+        JLog.logger.info("Installing linux EP...");
+
+        endpointLinux.InstallEPIncludingRequisites(AgentActions.EP_OS.LINUX, Integer.parseInt(general.get("EP " +
+                        "Installation timeout")), Integer.parseInt(general.get("EP Service Timeout")),
+                Integer.parseInt(general.get("From EP service start until logs show EP active timeout")));
+
+        // Sleep for 2 minutes - so the EPs will check updates and be updated
+        JLog.logger.info("Sleeping for 2 minutes so check updates would update the EPs...");
+        Thread.sleep(120000);
+
+        // Verify endpoint service is running
+        if(!endpointWin.EndPointServiceExist(AgentActions.EP_OS.WINDOWS))
+            org.testng.Assert.fail("Binary update failed. Trustwave Endpoint Agent Service not running on the windows EP.");
+        else
+            JLog.logger.info("Trustwave Endpoint Agent Service is running on the windows machine");
+
+        if(!endpointLinux.EndPointServiceExist(AgentActions.EP_OS.LINUX))
+            org.testng.Assert.fail("Binary update failed. Trustwave Endpoint Agent Service not running on the linux EP.");
+        else
+            JLog.logger.info("Trustwave Endpoint Agent Service is running on the linux machine");
+    }
+
+    private void FetchFilesFromNexusToLocal(String windowsNewVer, String linuxNewVer) throws IOException {
+        //String windowsNewVer = data.get("update_win_ver");
+        //String linuxNewVer = data.get("update_linux_ver");
+
+        List<String> windowsFiles = WindowsFilesList(windowsNewVer);
+        List<String> linuxFiles = LinuxFilesList(linuxNewVer);
+
+        String masterDownloadDirectory = PropertiesFile.getManagerDownloadFolder();
+        String localFolderForWinFiles = masterDownloadDirectory + "/windows/";
+        String localFolderForLinuxFiles = masterDownloadDirectory + "/linux/";
+
+        String fullPathToNexusWinVer = "https://nexus01.trustwave.com/content/repositories/releases/com/trustwave/nep/tmp/" + windowsNewVer + "/windows/";
+        String fullPathToNexusLinuxVer = "https://nexus01.trustwave.com/content/repositories/releases/com/trustwave/nep/tmp/" + linuxNewVer + "/linux/";
+
+        FetchFiles(fullPathToNexusWinVer, windowsFiles, localFolderForWinFiles);
+        FetchFiles(fullPathToNexusLinuxVer, linuxFiles, localFolderForLinuxFiles);
+    }
+
+    private void UploadFilesToBucket(String windowsNewVer, String linuxNewVer) {
+
+        String s3Bucket = data.get("s3Bucket");
+        //String windowsNewVer = data.get("update_win_ver");
+        //String linuxNewVer = data.get("update_linux_ver");
+
+        List<String> windowsFiles = WindowsFilesList(windowsNewVer);
+        List<String> linuxFiles = LinuxFilesList(linuxNewVer);
+
+        // Create folders on LNE machine
+        String winUpdFolderLNE = "/home/binaries/windows/";
+        String linuxUpdFolderLNE = "/home/binaries/linux/";
+
+        String masterDownloadDirectory = PropertiesFile.getManagerDownloadFolder();
+        String localFolderForWinFiles = masterDownloadDirectory + "/windows/";
+        String localFolderForLinuxFiles = masterDownloadDirectory + "/linux/";
+
+        if (!connection.IsFileExists("/home/binaries/")) {
+            connection.CreateDirectory("/home/binaries/");
+        }
+        if (!connection.IsFileExists(winUpdFolderLNE)) {
+            connection.CreateDirectory(winUpdFolderLNE);
+        }
+        if (!connection.IsFileExists(linuxUpdFolderLNE)) {
+            connection.CreateDirectory(linuxUpdFolderLNE);
+        }
+
+        // Copy all files from local folders to LNE
+        for (String file : windowsFiles) {
+            connection.CopyToRemote(localFolderForWinFiles + file, winUpdFolderLNE);
+        }
+        for (String file : linuxFiles) {
+            connection.CopyToRemote(localFolderForLinuxFiles + file, linuxUpdFolderLNE);
+        }
+
+        for (String file : windowsFiles) {
+            String response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
+                    "/services/stub-srv/etc/nep-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " +
+                    "AWS_SECRET_ACCESS_KEY=`grep aws-secret-access-key " + "/work/services/stub-srv/etc/nep" +
+                    "-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " + "AWS_REGION=`grep aws" +
+                    ".updates.bucket-region /work/services/stub-srv/etc/nep-properties/nepa-dserver" + ".properties |" +
+                    " cut -d '=' -f 2` ; cd /home/; s3_cmd upload_file " + s3Bucket + " binaries" +
+                    "/windows/" + file);
+            JLog.logger.info("Response: " + response);
+        }
+        for (String file : linuxFiles) {
+            String response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
+                    "/services/stub-srv/etc/nep-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " +
+                    "AWS_SECRET_ACCESS_KEY=`grep aws-secret-access-key " + "/work/services/stub-srv/etc/nep" +
+                    "-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " + "AWS_REGION=`grep aws" +
+                    ".updates.bucket-region /work/services/stub-srv/etc/nep-properties/nepa-dserver" + ".properties |" +
+                    " cut -d '=' -f 2` ; cd /home/; s3_cmd upload_file " + s3Bucket + " binaries" +
+                    "/linux/" + file);
+            JLog.logger.info("Response: " + response);
+        }
+    }
+
+    private void CleanBucket(String windowsNewVer, String linuxNewVer) {
+
+        String s3Bucket = data.get("s3Bucket");
+        //String windowsNewVer = data.get("update_win_ver");
+        //String linuxNewVer = data.get("update_linux_ver");
+
+        List<String> windowsFiles = WindowsFilesList(windowsNewVer);
+        List<String> linuxFiles = LinuxFilesList(linuxNewVer);
+
         JLog.logger.info("Cleaning the bucket now...");
         for (String file : windowsFiles) {
-            response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
+            String response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
                     "/services/stub-srv/etc/nep-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " +
                     "AWS_SECRET_ACCESS_KEY=`grep aws-secret-access-key " + "/work/services/stub-srv/etc/nep" +
                     "-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " + "AWS_REGION=`grep aws" +
@@ -188,7 +378,7 @@ public class BinaryUpdate extends GenericTest {
             JLog.logger.info("Response: " + response);
         }
         for (String file : linuxFiles) {
-            response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
+            String response = connection.Execute("export AWS_ACCESS_KEY_ID=`grep aws-access-key-id " + "/work" +
                     "/services/stub-srv/etc/nep-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " +
                     "AWS_SECRET_ACCESS_KEY=`grep aws-secret-access-key " + "/work/services/stub-srv/etc/nep" +
                     "-properties/nepa-dserver.properties | cut -d '=' -f 2` ; export " + "AWS_REGION=`grep aws" +
@@ -197,10 +387,38 @@ public class BinaryUpdate extends GenericTest {
                     "/linux/" + file);
             JLog.logger.info("Response: " + response);
         }
+    }
 
-        JLog.logger.info("Done...");
+    private void VerifySuccessfulUpdate(String windowsNewVer, String linuxNewVer) {
+        // Verify that the EPs have the correct new versions
+        String linuxBinVer = endpointLinux.getEPBinaryVersion(AgentActions.EP_OS.LINUX);
+        String winBinVer = endpointWin.getEPBinaryVersion(AgentActions.EP_OS.WINDOWS);
 
+        JLog.logger.info("Linux version is: " + linuxBinVer);
+        JLog.logger.info("Windows version is: " + winBinVer);
 
+        // Get newer binaries versions
+        //String windowsNewVer = data.get("update_win_ver");
+        //String linuxNewVer = data.get("update_linux_ver");
+
+        org.testng.Assert.assertEquals(linuxBinVer, linuxNewVer, "Versions don't match");
+        org.testng.Assert.assertEquals(winBinVer, windowsNewVer, "Versions don't match");
+    }
+
+    private void VerifySuccessfulRollback() {
+        // Verify that the EPs have the old versions
+        String linuxBinVer = endpointLinux.getEPBinaryVersion(AgentActions.EP_OS.LINUX);
+        String winBinVer = endpointWin.getEPBinaryVersion(AgentActions.EP_OS.WINDOWS);
+
+        JLog.logger.info("Linux version is: " + linuxBinVer);
+        JLog.logger.info("Windows version is: " + winBinVer);
+
+        // Get newer binaries versions
+        String windowsNewVer = data.get("update_win_ver");
+        String linuxNewVer = data.get("update_linux_ver");
+
+        org.testng.Assert.assertNotEquals(linuxBinVer, linuxNewVer, "Versions match, rollback wasn't performed");
+        org.testng.Assert.assertNotEquals(winBinVer, windowsNewVer, "Versions match, rollback wasn't performed");
     }
 
     private void FetchFiles(String nexusPath, List<String> files, String localFolder) throws IOException {
