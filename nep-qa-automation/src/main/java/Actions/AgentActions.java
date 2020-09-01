0d00863cd6203260174ace7dabb05b9faea2aeac
Dmitry Ryaboy <dryaboy@trustwave.com>
2020-09-01 18:53:40 +0300

    NEP-1260 - Delete End Point (Add DbJson object for reading data from db.json)

--- nep-qa-automation/src/main/java/Actions/AgentActions.java
+++ nep-qa-automation/src/main/java/Actions/AgentActions.java
@@ -1,11 +1,13 @@
 package Actions;
 
+import DataModel.DbJson;
 import Utils.EventsLog.LogEntry;
 import Utils.JsonUtil;
 import Utils.Logs.JLog;
 import Utils.PropertiesFile.PropertiesFile;
 import Utils.Remote.SSHManager;
 import Utils.TestFiles;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.SystemUtils;
@@ -651,8 +653,9 @@ public class AgentActions  {
             String dbJsonRemoteFile = (epOs == EP_OS.WINDOWS) ? dbJsonPath : dbJsonLinuxPath;
             if (connection.IsFileExists(dbJsonRemoteFile)) {
                 String dbJsonFileContent = connection.GetTextFromFile(dbJsonRemoteFile);
-                JSONObject json = new JSONObject(dbJsonFileContent);
-                endpointId = json.getString("EndpointId");
+                ObjectMapper objectMapper = new ObjectMapper();
+                DbJson dbJson = objectMapper.readValue(dbJsonFileContent, DbJson.class);
+                endpointId = dbJson.getEndpointId();
             }
         }
         catch (Exception e) {
