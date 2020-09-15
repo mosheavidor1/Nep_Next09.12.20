69b07f5e1cdfe20b5356b438b3e8a6ab6eb1157e
RTzukerman <rtzukerman@trustwave.com>
2020-09-15 14:49:13 +0300

    fixed ClusterConfiguration so simulated agents wont get bin update

--- nep-qa-automation/src/main/java/Tests/LNE/ClusterConfiguration.java
+++ nep-qa-automation/src/main/java/Tests/LNE/ClusterConfiguration.java
@@ -7,6 +7,7 @@ import Utils.JsonUtil;
 import Utils.Logs.JLog;
 import Utils.PropertiesFile.PropertiesFile;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
@@ -34,20 +35,19 @@ public class ClusterConfiguration extends GenericTest {
 
     @Test(groups = { "ClusterConfiguration" } )
     public void setClusterConfiguration() {
-
     	JLog.logger.info("Starting setClusterConfiguration test ...");
-    	
-    	lneActions = new LNEActions(PropertiesFile.readProperty("ClusterToTest"),general.get("LNE User Name"), general.get("LNE Password"), Integer.parseInt(general.get("LNE SSH port")));
+        try {
+        lneActions = new LNEActions(PropertiesFile.readProperty("ClusterToTest"),general.get("LNE User Name"), general.get("LNE Password"), Integer.parseInt(general.get("LNE SSH port")));
 
         String confJson =data.get("Settings Json");
 
         //delete simulated agents
         Map<String,List<String>> assignments = new HashMap<>();
 
-        simulatedAgentInCluster = new SimulatedAgentActions();
-        simulatedAgentInCluster.register(customerId, "1.2.3.4", ep1Name, "84-7B-EB-21-30","Windows 10");
-        simulatedAgentNotInCluster = new SimulatedAgentActions();
-        simulatedAgentNotInCluster.register(customerId, "1.2.3.5", ep2Name, "85-7B-EB-21-40","Windows 10");
+        simulatedAgentInCluster = new SimulatedAgentActions(customerId, "1.2.3.4", ep1Name, "84-7B-EB-21-99-99","Windows 10");
+        simulatedAgentNotInCluster = new SimulatedAgentActions(customerId, "1.2.3.5", ep2Name, "85-7B-EB-21-99-99","Windows 10");
+//        JLog.logger.info("sleeping 60 seconds until finish registering");
+//        Thread.sleep(60000);
 
         lneActions.setClusterConfig(customerId,clusterName, confJson);
 
@@ -67,8 +67,8 @@ public class ClusterConfiguration extends GenericTest {
         lneActions.setClusterConfig(customerId, clusterName, updatedClusterConfig);
 
 
-        String actionAgentInCluster = simulatedAgentInCluster.sendCheckUpdatesAndGetAction(simulatedAgentInCluster.getName(), "1.2.0.100", 1, 0, "1.1.1");
-        String actionAgentNotInCluster = simulatedAgentNotInCluster.sendCheckUpdatesAndGetAction(simulatedAgentNotInCluster.getName(), "1.2.0.100", 1, 0, "1.1.1");
+        String actionAgentInCluster = simulatedAgentInCluster.sendCheckUpdatesAndGetAction(simulatedAgentInCluster.getName(), "9.9.9.999", 1, 0, "1.1.1");
+        String actionAgentNotInCluster = simulatedAgentNotInCluster.sendCheckUpdatesAndGetAction(simulatedAgentNotInCluster.getName(), "9.9.9.999", 1, 0, "1.1.1");
 
 
         org.testng.Assert.assertTrue(actionAgentInCluster.contains("switch"),"setClusterConfiguration test failed, ep added to cluster should have received configuration switch when checking update, action: "+actionAgentInCluster);
@@ -94,7 +94,7 @@ public class ClusterConfiguration extends GenericTest {
         assignments.put(clusterName,new LinkedList<>());
         lneActions.updateClusterMap(Long.valueOf(customerId), assignments);
 
-        actionAgentInCluster = simulatedAgentInCluster.sendCheckUpdatesAndGetAction(simulatedAgentInCluster.getName(), "1.2.0.100", 0, 0, "1.1.1");
+        actionAgentInCluster = simulatedAgentInCluster.sendCheckUpdatesAndGetAction(simulatedAgentInCluster.getName(), "9.9.9.999", 0, 0, "1.1.1");
         org.testng.Assert.assertTrue(actionAgentInCluster.contains("switch"),"setClusterConfiguration test failed, ep added to cluster should have received configuration switch when checking update");
 
 
