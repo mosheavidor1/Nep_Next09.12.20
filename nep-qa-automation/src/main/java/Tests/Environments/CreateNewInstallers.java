b40a8f4ea30d099de3d7031f119085311786c53d
Uri Hacohen <uri.hacohen@trustwave.com>
2020-10-28 14:43:23 +0200

    Workaround of Selenium fail to work with the same file cabinet page at the same test

---
+++ nep-qa-automation/src/main/java/Tests/Environments/CreateNewInstallers.java
@@ -0,0 +1,57 @@
+package Tests.Environments;
+
+import Actions.BrowserActions;
+import Tests.RecordedTest;
+import Utils.Logs.JLog;
+
+import org.testng.annotations.*;
+
+
+public class CreateNewInstallers extends RecordedTest {
+    private BrowserActions action;
+
+    @Factory(dataProvider = "getData")
+    public CreateNewInstallers(Object dataToSet) {
+        super(dataToSet);
+        action = new BrowserActions();
+
+    }
+
+    @Test(groups = { "CreateNewInstallers" } )
+    public void CreateNewInstalllersAndDeleteOldInstallers () {
+        try {
+
+            JLog.logger.info("Starting Create New IOnstallers...");
+
+            action.LaunchApplication(getGeneralData().get("Browser"));
+            action.SetApplicationUrl(getGeneralData().get("Fusion Link"));
+
+            action.Login(getGeneralData().get("Fusion User Name"), getGeneralData().get("Fusion Password"));
+
+            action.GotoPortalFileCabinetPage(getGeneralData().get("Fusion Link"));
+            action.SelectCustomer(data.get("Customer"));
+            action.DeleteAllDownloads();
+
+
+            action.GotoCentComSearch(getGeneralData().get("Fusion Link"));
+            action.PublishNewDownloads(data.get("Customer"), Integer.parseInt(data.get("Wait for publish to be completed")));
+        }
+        catch (Exception e){
+            org.testng.Assert.fail("Could not create new installers " +e.toString());
+        }
+
+
+    }
+
+    @AfterMethod
+    public void Close() throws Exception {
+        afterMethod();
+        if (action != null) {
+            action.CloseApplication();
+        }
+
+    }
+
+
+
+}
---
+++ nep-qa-automation/src/main/java/Tests/Environments/DownloadInstallers.java
@@ -0,0 +1,62 @@
+package Tests.Environments;
+
+import Actions.BrowserActions;
+import Tests.RecordedTest;
+import Utils.Logs.JLog;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+
+public class DownloadInstallers extends RecordedTest {
+    private BrowserActions action;
+
+    @Factory(dataProvider = "getData")
+    public DownloadInstallers(Object dataToSet) {
+        super(dataToSet);
+        action = new BrowserActions();
+
+    }
+
+    @Test(groups = { "DownloadInstallers" } )
+    public void downloadInstallersFromFileCabinet () {
+        try {
+            JLog.logger.info("Starting download Installers From File Cabinet...");
+
+            action.CreateAndCleanDownloadFolder();
+
+            action.LaunchApplication(getGeneralData().get("Browser"));
+            action.SetApplicationUrl(getGeneralData().get("Fusion Link"));
+
+            action.Login(getGeneralData().get("Fusion User Name"), getGeneralData().get("Fusion Password"));
+
+            action.GotoPortalFileCabinetPage(getGeneralData().get("Fusion Link"));
+            action.SelectCustomer(data.get("Customer"));
+
+            action.DownloadFilesFromTrustWaveEndPointFolder(data.get("Wait files ready to download timeout"), data.get("Wait because File is still being processed (virus scanned and stored)"));
+
+            action.VerifyFilesExist(Integer.parseInt(data.get("Wait files to be downloaded timeout in seconds")));
+
+            action.VerifyInstallerSignature();
+
+            JLog.logger.info("Finished selectCustomerDownloadInstaller.");
+        }
+        catch (Exception e){
+            org.testng.Assert.fail("Could not create download installers from file cabinet " +e.toString());
+        }
+
+
+    }
+
+    @AfterMethod
+    public void Close() throws Exception {
+        afterMethod();
+        if (action != null) {
+            action.CloseApplication();
+        }
+
+    }
+
+
+
+}
