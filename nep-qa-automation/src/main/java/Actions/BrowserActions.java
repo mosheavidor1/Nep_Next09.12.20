e6e49688c83e369f6be6a02a87a2ba1005b1e0ec
Uri Hacohen <uri.hacohen@trustwave.com>
2020-12-08 19:22:03 +0200

    MSI installer appears late in file cabinet. Added a wait for MSI link appearance

--- nep-qa-automation/src/main/java/Actions/BrowserActions.java
+++ nep-qa-automation/src/main/java/Actions/BrowserActions.java
@@ -571,6 +571,7 @@ public class BrowserActions extends ManagerActions {
             int fileStoredAndVirusScanTimeout = Integer.parseInt(fileStoredAndVirusScanTimeoutString);
 
             boolean errorMessageAppear = true;
+            boolean installerAppear = false;
             FileCabinet fc = new FileCabinet();
 
             LocalDateTime start = LocalDateTime.now();
@@ -579,14 +580,17 @@ public class BrowserActions extends ManagerActions {
 
             while (durationTimeout.compareTo(Duration.between(start, current)) > 0) {
 
-                fc.WaitUntilObjectClickable(installerLinkBy);
-                installerLink.click();
-                if (!fc.IsElementExist(fc.fileUnableToBeDownloadedBy)) {
-                    errorMessageAppear = false;
-                    break;
-                }
+                if(fc.IsElementExist(installerLinkBy)) {
+                    installerAppear = true;
+                    fc.WaitUntilObjectClickable(installerLinkBy);
+                    installerLink.click();
+                    if (!fc.IsElementExist(fc.fileUnableToBeDownloadedBy)) {
+                        errorMessageAppear = false;
+                        break;
+                    }
 
-                fc.errorMessageOKButton_element.click();
+                    fc.errorMessageOKButton_element.click();
+                }
 
                 fc.refreshButton_element.click();
 
@@ -599,6 +603,10 @@ public class BrowserActions extends ManagerActions {
                 fc.WaitUntilObjectClickable(fc.refreshButtonBy);
 
 
+            }
+
+            if (installerAppear==false) {
+                org.testng.Assert.fail("Installer link did not appeared at file cabinet:  "+ installerLinkBy + " .After timeout: "+ fileStoredAndVirusScanTimeoutString);
             }
 
             if (errorMessageAppear) {
